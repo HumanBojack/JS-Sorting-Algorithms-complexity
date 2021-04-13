f717afe43b46d1d9de0e74fc15e883df67f7b0b8
HumanBojack <spychalar59@gmail.com>
2021-04-13 13:59:53 +0200

    Bubble sort

--- counter.js
+++ counter.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 class Counter {
 	constructor() {
-		this.numbers = this.getData(process.argv[2])
+		this.numbers = this.testData(this.getData(process.argv[2]));
+		this.bubbleSortComparaisons = 0;
 	}
 
 	getData(fileName) {
@@ -10,7 +11,7 @@ class Counter {
 			return data;
 		} catch (error) {
 			console.error(error.message);
-			process.exit()
+			process.exit();
 		}
 	}
 
@@ -18,16 +19,41 @@ class Counter {
 		let dataArray;
 		try { 
 			dataArray = JSON.parse(data);
-			return dataArray;
 		} catch (error) {
 			console.error(error.message);
-			process.exit()
+			process.exit();
 		}
+
+		if (Array.isArray(dataArray) && dataArray.every(number => typeof number == "number") && dataArray.length > 0){
+			return dataArray;
+		} else {
+			console.error("Your input is not valid, please provide an array of integers.");
+			process.exit();
+		}
+	}
+
+	bubbleSort(){
+		let numbers = [...this.numbers];
+		let arrayIsModified = true;
+
+		while (arrayIsModified == true){
+			arrayIsModified = false;
+			for(let i = 1; i < numbers.length; i++){
+				this.bubbleSortComparaisons += 1;
+				if (numbers[i-1] > numbers[i]){
+					[numbers[i-1],numbers[i]] = [numbers[i],numbers[i-1]];
+					arrayIsModified = true;
+				}
+			}
+		}
+		
+		return numbers;
 	}
 
 
 }
 
 c = new Counter
-console.log(c.numbers)
-console.log(c.testData(c.numbers));
+
+console.log(c.bubbleSort());
+console.log(c.bubbleSortComparaisons)
